acf62e6004069fa182e52164adb33f807b4664db
Randy Kim <rhkim1292@gmail.com>
2023-01-11 01:49:13 -0800

    Add a few products in the shop

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Home from './components/Home';
 import Shop from './components/Shop';
+import indianScoutRogue from './images/indian-scout-rogue.png';
+import ninjaH2 from './images/ninja-h2.png';
+import yzfR1m from './images/yzf-r1m.png';
+import diavelV4 from './images/diavel-v4.png';
 
 function App() {
 	const [cart, setCart] = useState([]);
-	const [shopItems, setShopItems] = useState([
+	const shopItems = [
 		{
-			name: 'test item',
-			price: 100,
+			name: '2023 Indian Scout Rogue',
+			price: 14149,
+			imgPath: indianScoutRogue,
 		},
-	]);
+		{
+			name: '2023 Kawasaki Ninja H2',
+			price: 31500,
+			imgPath: ninjaH2,
+		},
+		{
+			name: '2023 Yamaha YZF-R1M',
+			price: 26999,
+			imgPath: yzfR1m,
+		},
+		{
+			name: '2023 Ducati Diavel V4',
+			price: 26695,
+			imgPath: diavelV4,
+		},
+	];
 
 	useEffect(() => {});
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -3,8 +3,16 @@ import '../styles/Item.css';
 function Item(props) {
 	return (
 		<div className="item-container">
-			<h2>{props.item.name}</h2>
-			<h1>${props.item.price}</h1>
+			<img
+				className="item-img"
+				src={props.item.imgPath}
+				alt={props.item.name}
+			/>
+			<div className="item-details">
+				<h2>{props.item.name}</h2>
+				<h3>MSRP</h3>
+				<h1>${props.item.price}</h1>
+			</div>
 		</div>
 	);
 }
